a4e9c9dc82717d67842ba174e50727642ffda9f0
Alex Morega <alex@grep.ro>
2014-07-29 09:32:24 +0200

    load cdnjs url from settings

--- zechat/app.py
+++ zechat/app.py
@@ -1,11 +1,19 @@
 import flask
 
 
+DEFAULTS = {
+    'CDNJS_URL': '//cdnjs.cloudflare.com/ajax/libs',
+}
+
+
 def create_app():
     from zechat import models
     from zechat.node import views as node_views, websocket
     from zechat.common import views as common_views, assets
     app = flask.Flask(__name__)
+    app.config.update(DEFAULTS)
+    app.config.from_pyfile('../settings.py', silent=False)
+    app.jinja_env.globals['cdnjs'] = app.config['CDNJS_URL']
     models.db.init_app(app)
     assets.init_app(app)
     websocket.init_app(app)
