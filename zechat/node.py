ac5fd54c93086aed025594cfefa50113ebdbe40d
Alex Morega <alex@grep.ro>
2014-07-29 23:31:53 +0300

    extract the input loop

--- zechat/node.py
+++ zechat/node.py
@@ -11,18 +11,22 @@ class Transport(object):
     def __init__(self, ws):
         self.ws = ws
 
+    def messages(self):
+        while True:
+            msg = self.ws.receive()
+            if msg is None:  # disconnect
+                break
+
+            if not msg:  # ping?
+                continue
+
+            logger.debug("message: %s", msg)
+            yield msg
+
     def run(self):
         client_map[self.ws.id] = self.ws
         try:
-            while True:
-                msg = self.ws.receive()
-                if msg is None:  # disconnect
-                    break
-
-                if not msg:  # ping?
-                    continue
-
-                logger.debug("message: %s", msg)
+            for msg in self.messages():
                 for client in client_map.values():
                     client.send(msg)
 
