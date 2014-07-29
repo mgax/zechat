e314c955ba272b0e68bd1bae66a5d9e0f901b788
Alex Morega <alex@grep.ro>
2014-07-29 23:07:51 +0300

    remove obsolete views

--- zechat/app.py
+++ zechat/app.py
@@ -8,7 +8,7 @@ DEFAULTS = {
 
 def create_app():
     from zechat import models
-    from zechat.node import views as node_views, websocket
+    from zechat.node import websocket
     from zechat.common import views as common_views, assets
     app = flask.Flask(__name__)
     app.config.update(DEFAULTS)
@@ -17,7 +17,6 @@ def create_app():
     models.db.init_app(app)
     assets.init_app(app)
     websocket.init_app(app)
-    app.register_blueprint(node_views)
     app.register_blueprint(common_views)
     return app
 
--- zechat/node.py
+++ zechat/node.py
@@ -1,34 +1,11 @@
 import logging
-import flask
-from zechat.models import db, Message
 from flask.ext.uwsgi_websocket import GeventWebSocket
 
 logger = logging.getLogger(__name__)
 
 websocket = GeventWebSocket()
 
-views = flask.Blueprint('node', __name__)
-
 client_map = {}
-
-
-@views.route('/save', methods=['POST'])
-def save():
-    data = flask.request.get_json()
-    message = Message(recipient=data['recipient'], data=flask.json.dumps(data))
-    db.session.add(message)
-    db.session.commit()
-    return 'ok'
-
-
-@views.route('/get_messages', methods=['POST'])
-def get_messages():
-    identity = flask.request.get_json()['identity']
-    message_list = [
-        flask.json.loads(m.data)
-        for m in Message.query.filter_by(recipient=identity)
-    ]
-    return flask.jsonify(message_list=message_list)
 
 
 @websocket.route('/ws/transport')
