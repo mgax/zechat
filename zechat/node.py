ca01114cac580c1310b36fe02f09a56257fcace0
Alex Morega <alex@grep.ro>
2014-07-29 23:30:20 +0300

    refactor transport as class

--- zechat/node.py
+++ zechat/node.py
@@ -6,23 +6,32 @@ logger = logging.getLogger(__name__)
 client_map = {}
 
 
+class Transport(object):
+
+    def __init__(self, ws):
+        self.ws = ws
+
+    def run(self):
+        client_map[self.ws.id] = self.ws
+        try:
+            while True:
+                msg = self.ws.receive()
+                if msg is None:  # disconnect
+                    break
+
+                if not msg:  # ping?
+                    continue
+
+                logger.debug("message: %s", msg)
+                for client in client_map.values():
+                    client.send(msg)
+
+        finally:
+            del client_map[self.ws.id]
+
+
 def transport(ws):
-    client_map[ws.id] = ws
-    try:
-        while True:
-            msg = ws.receive()
-            if msg is None:  # disconnect
-                break
-
-            if not msg:  # ping?
-                continue
-
-            logger.debug("message: %s", msg)
-            for client in client_map.values():
-                client.send(msg)
-
-    finally:
-        del client_map[ws.id]
+    Transport(ws).run()
 
 
 def init_app(app):
