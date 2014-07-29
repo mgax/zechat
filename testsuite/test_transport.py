c4d3a322082f4cf5e95f780f58c70038e6bdf911
Alex Morega <alex@grep.ro>
2014-07-29 23:21:44 +0300

    extract testing helper

--- testsuite/test_transport.py
+++ testsuite/test_transport.py
@@ -1,12 +1,13 @@
 from mock import Mock, call
 
 
-def test_roundtrip():
+def run_transport(incoming, client_id='one'):
     from zechat.node import transport
-
-    ws = Mock(id='one')
-    ws.receive.side_effect = ['foo', 'bar', None]
-
+    ws = Mock(id=client_id)
+    ws.receive.side_effect = incoming + [None]
     transport(ws)
+    return [c[0][0] for c in ws.send.call_args_list]
 
-    assert ws.send.call_args_list == [call('foo'), call('bar')]
+
+def test_roundtrip():
+    assert run_transport(['foo', 'bar']) == ['foo', 'bar']
