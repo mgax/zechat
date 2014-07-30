c0375ed29adea98e5708fdd0fb73c9da30508ebc
Alex Morega <alex@grep.ro>
2014-07-30 15:06:43 +0300

    remove obsolete model

--- zechat/models.py
+++ zechat/models.py
@@ -1,10 +1,3 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-
-class Message(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    recipient = db.Column(db.String, nullable=False)
-    data = db.Column(db.String, nullable=False)
