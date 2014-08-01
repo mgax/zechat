c146795ac1cce16b837313946b559d3ccd2ca41b
Alex Morega <alex@grep.ro>
2014-08-01 16:38:09 +0300

    set up flask-migrate

--- zechat/app.py
+++ zechat/app.py
@@ -23,6 +23,11 @@ def create_app(**config):
 
 def create_manager(app):
     from flask.ext.script import Manager
+    from flask.ext.migrate import Migrate, MigrateCommand
+    from zechat.models import db
+
+    migrate = Migrate(app, db)
 
     manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
     return manager
