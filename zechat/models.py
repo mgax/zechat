17338a6c8be60c40e0db0a97e2d35f16c18be44d
Alex Morega <alex@grep.ro>
2014-08-07 18:40:49 +0300

    'message' table

--- zechat/models.py
+++ zechat/models.py
@@ -8,3 +8,10 @@ class Identity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fingerprint = db.Column(db.String, nullable=False, unique=True, index=True)
     public_key = db.Column(db.String, nullable=False)
+
+
+class Message(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipient = db.Column(db.String, nullable=False, unique=True, index=True)
+    payload = db.Column(db.String, nullable=False)
