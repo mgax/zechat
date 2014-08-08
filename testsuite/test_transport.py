fc4b2486e4ad78b50e8aea60176ee5f3c21be81b
Alex Morega <alex@grep.ro>
2014-08-08 12:06:55 +0300

    wrap mock websocket client into class

--- testsuite/test_transport.py
+++ testsuite/test_transport.py
@@ -32,6 +32,13 @@ def mock_ws(client_id):
     return ws
 
 
+class Client(object):
+
+    def __init__(self, ws_id):
+        self.ws = mock_ws(ws_id)
+        self.out = self.ws.out
+
+
 def msg(recipient, text):
     return dict(type='message', recipient=recipient, message=dict(text=text))
 
@@ -53,19 +60,19 @@ def msghash(text):
 
 
 def test_loopback(node):
-    ws = mock_ws('A')
-    with node.transport(ws) as transport:
+    peer = Client('A')
+    with node.transport(peer.ws) as transport:
         transport.packet(auth('A'))
         transport.packet(msg('A', 'foo'))
         transport.packet(msg('A', 'bar'))
-    assert ws.out == [msg('A', 'foo'), msg('A', 'bar')]
+    assert peer.out == [msg('A', 'foo'), msg('A', 'bar')]
 
 
 def test_peer_receives_messages(node):
-    peer = mock_ws('B')
-    with node.transport(peer) as peer_transport:
+    peer = Client('B')
+    with node.transport(peer.ws) as peer_transport:
         peer_transport.packet(auth('B'))
-        with node.transport(mock_ws('A')) as sender_transport:
+        with node.transport(Client('A').ws) as sender_transport:
             sender_transport.packet(msg('B', 'foo'))
             sender_transport.packet(msg('B', 'bar'))
 
@@ -73,13 +80,13 @@ def test_peer_receives_messages(node):
 
 
 def test_messages_filtered_by_recipient(node):
-    a = mock_ws('A')
-    b = mock_ws('B')
-    with node.transport(a) as tr_a, node.transport(b) as tr_b:
+    a = Client('A')
+    b = Client('B')
+    with node.transport(a.ws) as tr_a, node.transport(b.ws) as tr_b:
         tr_a.packet(auth('A'))
         tr_b.packet(auth('B'))
 
-        with node.transport(mock_ws('sender')) as sender_transport:
+        with node.transport(Client('sender').ws) as sender_transport:
             sender_transport.packet(msg('A', 'foo'))
             sender_transport.packet(msg('B', 'bar'))
 
@@ -88,12 +95,12 @@ def test_messages_filtered_by_recipient(node):
 
 
 def test_message_history(node):
-    with node.transport(mock_ws('A')) as tr_a:
+    with node.transport(Client('A').ws) as tr_a:
         tr_a.packet(msg('B', 'foo'))
         tr_a.packet(msg('B', 'bar'))
 
-    b = mock_ws('B')
-    with node.transport(b) as tr_b:
+    b = Client('B')
+    with node.transport(b.ws) as tr_b:
         tr_b.packet(list_('B'))
         assert b.out == [{'messages': [msghash('foo'), msghash('bar')]}]
         b.out[:] = []
