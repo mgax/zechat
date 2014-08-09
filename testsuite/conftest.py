dd54ccd5c27327b1e80da35c0bf86a247da0d2cf
Alex Morega <alex@grep.ro>
2014-08-09 22:26:04 +0300

    create app0 only once

--- testsuite/conftest.py
+++ testsuite/conftest.py
@@ -2,14 +2,15 @@ import pytest
 import flask
 
 
+app0 = flask.Flask(__name__)
+app0.config.from_pyfile('../settings.py', silent=False)
+db_uri = app0.config['TESTING_SQLALCHEMY_DATABASE_URI']
+del app0
+
+
 @pytest.fixture
 def app():
     from zechat import models
-
-    app0 = flask.Flask(__name__)
-    app0.config.from_pyfile('../settings.py', silent=False)
-    db_uri = app0.config['TESTING_SQLALCHEMY_DATABASE_URI']
-
     app = flask.Flask(__name__)
     app.testing = True
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
