b04f06084eb0c24cac71339d06fe4b8bed3cf38a
Alex Morega <alex@grep.ro>
2014-08-10 11:30:28 +0300

    wrap auth and msg composition into Identity class

--- testsuite/test_transport.py
+++ testsuite/test_transport.py
@@ -50,16 +50,40 @@ class Client(object):
             yield self
 
 
-def msg(recipient, text):
-    return dict(type='message', recipient=recipient, message=dict(text=text))
+class Identity(object):
+
+    def __init__(self, key=None):
+        if key is None:
+            from test_crypto import PRIVATE_KEY
+            key = PRIVATE_KEY
+
+        from zechat.cryptos import Crypto
+        self.crypto = Crypto(key)
+
+    @property
+    def fp(self):
+        return self.crypto.fingerprint()
+
+    def authenticate(self, peer):
+        peer.send(dict(type='challenge'), 1)
+        challenge = peer.out.pop()['challenge']
+        public_key = self.crypto.public_key()
+        response = json.dumps(dict(challenge=challenge, public_key=public_key))
+        auth_packet = dict(
+            type='authenticate',
+            response=response,
+            signature=self.crypto.sign(response),
+        )
+        peer.send(auth_packet, 2)
+        assert peer.out.pop()['success'] == True
+
+    def message(self, recipient, text):
+        payload = dict(text=text)
+        return dict(type='message', recipient=recipient, message=payload)
 
 
 def reply(serial, **data):
     return dict(_reply=serial, **data)
-
-
-def auth(response, signature):
-    return dict(type='authenticate', response=response, signature=signature)
 
 
 def subscribe(identity):
@@ -78,82 +102,79 @@ def msghash(text):
     return sha1(json.dumps(dict(text=text))).hexdigest()
 
 
-def authenticate(peer, key=None):
-    from zechat.cryptos import Crypto
-
-    if key is None:
-        from test_crypto import PRIVATE_KEY
-        key = PRIVATE_KEY
-
-    peer.send(dict(type='challenge'), 1)
-    challenge = peer.out.pop()['challenge']
-    crypto = Crypto(key)
-    public_key = crypto.public_key()
-    response = json.dumps(dict(challenge=challenge, public_key=public_key))
-    peer.send(auth(response, crypto.sign(response)), 2)
-    assert peer.out.pop()['success'] == True
-    return crypto.fingerprint()
-
-
 def test_loopback(node):
+    id = Identity()
     with connection(node) as peer:
-        fp = authenticate(peer)
-        peer.send(subscribe(fp), 2)
-        peer.send(msg(fp, 'foo'), 3)
-        peer.send(msg(fp, 'bar'), 4)
+        id.authenticate(peer)
+        foo = id.message(id.fp, 'foo')
+        bar = id.message(id.fp, 'bar')
+
+        peer.send(subscribe(id.fp), 2)
+        peer.send(foo, 3)
+        peer.send(bar, 4)
 
     assert peer.out == [
         reply(2),
-        msg(fp, 'foo'), reply(3),
-        msg(fp, 'bar'), reply(4),
+        foo, reply(3),
+        bar, reply(4),
     ]
 
 
 def test_peer_receives_messages(node):
+    id = Identity()
     with connection(node) as peer:
-        fp = authenticate(peer)
-        peer.send(subscribe(fp), 2)
+        id.authenticate(peer)
+        peer.send(subscribe(id.fp), 2)
+        foo = id.message(id.fp, 'foo')
+        bar = id.message(id.fp, 'bar')
 
         with connection(node) as sender:
-            peer.send(msg(fp, 'foo'), 3)
-            peer.send(msg(fp, 'bar'), 4)
+            peer.send(foo, 3)
+            peer.send(bar, 4)
 
     assert peer.out == [
         reply(2),
-        msg(fp, 'foo'), reply(3),
-        msg(fp, 'bar'), reply(4),
+        foo, reply(3),
+        bar, reply(4),
     ]
 
 
 def test_messages_filtered_by_recipient(node):
-    from test_crypto import PRIVATE_KEY_B, FINGERPRINT_B as fp_b
+    from test_crypto import PRIVATE_KEY_B
+    id = Identity()  # sender
+    id_a = Identity()  # recipient A
+    id_b = Identity(PRIVATE_KEY_B)  # recipient B
     with connection(node) as a, connection(node) as b:
-        fp_a = authenticate(a)
-        a.send(subscribe(fp_a), 2)
-        assert authenticate(b, PRIVATE_KEY_B) == fp_b
-        b.send(subscribe(fp_b), 2)
+        id_a.authenticate(a)
+        a.send(subscribe(id_a.fp), 2)
+        id_b.authenticate(b)
+        b.send(subscribe(id_b.fp), 2)
+        foo = id.message(id_a.fp, 'foo')
+        bar = id.message(id_b.fp, 'bar')
 
         with connection(node) as sender:
-            sender.send(msg(fp_a, 'foo'))
-            sender.send(msg(fp_b, 'bar'))
+            sender.send(foo)
+            sender.send(bar)
 
-    assert a.out == [reply(2), msg(fp_a, 'foo')]
-    assert b.out == [reply(2), msg(fp_b, 'bar')]
+    assert a.out == [reply(2), foo]
+    assert b.out == [reply(2), bar]
 
 
 def test_message_history(node):
-    from test_crypto import PRIVATE_KEY_B, FINGERPRINT_B as fp_b
+    id = Identity()
+    foo = id.message(id.fp, 'foo')
+    bar = id.message(id.fp, 'bar')
 
     with connection(node) as a:
-        a.send(msg(fp_b, 'foo'))
-        a.send(msg(fp_b, 'bar'))
+        a.send(foo)
+        a.send(bar)
 
     with connection(node) as b:
-        assert authenticate(b, PRIVATE_KEY_B) == fp_b
+        id.authenticate(b)
 
-        b.send(list_(fp_b), 2)
+        b.send(list_(id.fp), 2)
         assert b.out == [reply(2, messages=[msghash('foo'), msghash('bar')])]
         b.out[:] = []
 
-        b.send(get(fp_b, [msghash('foo'), msghash('bar')]), 3)
-        assert b.out == [reply(3, messages=[msg(fp_b, 'foo'), msg(fp_b, 'bar')])]
+        b.send(get(id.fp, [msghash('foo'), msghash('bar')]), 3)
+        assert b.out == [reply(3, messages=[foo, bar])]
