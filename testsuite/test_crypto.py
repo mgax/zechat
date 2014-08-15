487574e1a51635c9eadcd7e6f799b78bdc9ccd09
Alex Morega <alex@grep.ro>
2014-08-15 21:02:07 +0300

    Curve25519 public key encryption in python

--- testsuite/test_crypto.py
+++ testsuite/test_crypto.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 PRIVATE_KEY = """\
 -----BEGIN RSA PRIVATE KEY-----
 MIICXQIBAAKBgQCdevmtfX+x4lwQSEZbpBhTd/aErOeRDXNhDC6Ynl4ifpqU4dlP
@@ -66,9 +69,24 @@ r1fapTBzsGosdKg7sNBH8am/zPGZEMJia9qfoJiNmUs=
 FINGERPRINT_B = 'f9df70f799f933fe997312012a7ed369333631a6'
 
 
+A_KEY = 'VrIuRMeVZkmqlS9Sa9VRritZ1eVmnyJcZZFKJUkdnvk='
+A_PUBKEY = 'B8dnDDjozeRUBsMFlPiWL4HR6kLEa9WyVRga4Q/CoXY='
+
+B_KEY = 'NuwWzeSWynTxvBfNxi1z5UwG7AtKwwQYpW0GlDde4Fs='
+B_PUBKEY = 'YCBnGbI2GbfWjmJl22o4IH3sIACU8Sv58fcxfDQojhI='
+
+A_B_ENCRYPTED = 'zc+OgEhoQm3Yu8vqsFcuvzc0FJuQ2au4+wrxt8hGkss1jDAFXEMRoRU6+g=='
+
+
 def crypto(key):
     from zechat.cryptos import Crypto
     return Crypto(key)
+
+
+@pytest.fixture(scope='module')
+def curve():
+    from zechat.cryptos import CurveCrypto
+    return CurveCrypto()
 
 
 def test_decrypt():
@@ -102,3 +120,27 @@ def test_fingerprint():
 
 def test_public_key():
     assert crypto(PRIVATE_KEY).public_key() == PUBLIC_KEY
+
+
+def test_curve_encryption(curve):
+    assert curve.decrypt(A_B_ENCRYPTED, A_PUBKEY, B_KEY) == 'foo'
+
+    encrypted = curve.encrypt('foo', A_KEY, B_PUBKEY)
+    assert curve.decrypt(encrypted, A_PUBKEY, B_KEY) == 'foo'
+
+
+def test_curve_decryption_errors(curve):
+    from zechat.cryptos import DecryptionError
+
+    with pytest.raises(DecryptionError):
+        assert curve.decrypt(A_B_ENCRYPTED, A_PUBKEY, A_KEY)
+
+    with pytest.raises(DecryptionError):
+        assert curve.decrypt('asdf'*20, A_PUBKEY, B_KEY)
+
+
+def test_curve_nonces_are_different(curve):
+    nonce_set = set()
+    for _ in range(100):
+        nonce_set.add(curve.nonce())
+    assert len(nonce_set) == 100
--- zechat/cryptos.py
+++ zechat/cryptos.py
@@ -1,9 +1,14 @@
 from base64 import b64encode, b64decode
 import hashlib
+import time
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Hash import SHA256
 from Crypto.Signature import PKCS1_PSS
+import nacl.utils
+from nacl.public import PrivateKey, PublicKey, Box
+from nacl.encoding import Base64Encoder
+from nacl.exceptions import CryptoError
 
 
 class Crypto(object):
@@ -36,3 +41,36 @@ class Crypto(object):
 
     def public_key(self):
         return self.key.publickey().exportKey() + '\n'
+
+
+class DecryptionError(Exception):
+    """ Failed to decypt message. """
+
+
+class CurveCrypto(object):
+
+    def __init__(self):
+        self.last_nonce = nacl.utils.random(Box.NONCE_SIZE)
+
+    def nonce(self):
+        hash = hashlib.sha512(self.last_nonce + str(time.time()))
+        new_nonce = hash.digest()[:Box.NONCE_SIZE]
+        self.last_nonce = new_nonce
+        return new_nonce
+
+    def encrypt(self, message, sender, recipient_pub):
+        box = Box(
+            PrivateKey(sender, Base64Encoder),
+            PublicKey(recipient_pub, Base64Encoder),
+        )
+        return box.encrypt(message, self.nonce(), encoder=Base64Encoder)
+
+    def decrypt(self, encrypted, sender_pub, recipient):
+        box = Box(
+            PrivateKey(recipient, Base64Encoder),
+            PublicKey(sender_pub, Base64Encoder),
+        )
+        try:
+            return box.decrypt(encrypted, encoder=Base64Encoder)
+        except CryptoError:
+            raise DecryptionError
